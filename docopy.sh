0ea3154b67642a1acd5b5347ec05536cb3f1c7ae
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-03-12 18:05:27 -0300

    build: adiciona as principais funções de backup da home ao script
    Este commit adiciona as principais funções: copia a pasta home, gera um arquivo tar.gz desta cópia; adiciona as alterações e faz o commit; faz push da nova versão do backup para o repositório remoto;

--- docopy.sh
+++ docopy.sh
@@ -1,17 +1,48 @@
-# Exclui a última cópia da minha pasta home
-rm -r /fabricio
+# Define o caminho para a pasta home
+homedir=""
+
+# Define a url do repositório remoto
+remote_repo=""
+# ----------------------------------------------------------------------
+# Cria um diretório temporário para colocar a cópia da home
+mkdir ./backup/copia
 
 # Faz uma nova cópia
-cp -r /home/fabricio/ ./
+cp -r $homedir ./backup/copia
+
+# Salva o nome da pasta copiada
+homename=$(ls ./backup/copia/)
+
+# Gera um pacote TAR comprimido em GZIP de todos os arquivos presentes na pasta copiada
+tar -czf ./backup/$homename-home-backup.tar.gz ./backup/copia/$homename/*
+
+# Remove a pasta de cópia temporária
+sudo rm -r ./backup/copia
 
 # Busca qual foi a última versão do backup
-lastVersion=$(cat backup_lastVersion.txt)
+lastVersion=$(cat ./sources/backup_lastVersion.txt)
 
 # Gera a nova versão do commit que será feito
-newVersion=$((lastVersion+1))
+newVersion=$(($lastVersion+1))
+
+if [ ! -d "./.git" ]; then
+	# Inicia o git
+	git init > ./sources/gitlog.txt
+	
+	# Adiciona o repositório remoto
+	git remote add origin $remote_repo >> ./sources/gitlog.txt
+fi
 
 # Adiciona as mudanças para serem comitadas
-git add fabricio-test/
+git add ./backup >> ./sources/gitlog.txt
 
 # Faz o commit incluindo a nova versão na mensagem do commit
-git commit -m "backup: /home/fabricio v$newVersion.0"
+git commit -m "backup: /home/$homename v$newVersion.0" >> ./sources/gitlog.txt
+
+# Manda o novo backup para o repositório remoto
+git push origin main >> ./sources/gitlog.txt
+
+# Atualiza a versão do backup salvo no arquivo
+echo $newVersion > ./sources/backup_lastVersion.txt
+
+echo "Backup do diretório /home/$homename feito!"
