1e336659ac9cc084ee2ce51501ebcb3411257be0
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-03-13 23:45:12 -0300

    build: adiciona o script que inicia o sistema
    Este commit implementa o script qe dá início ao sistema. O script start.sh configura o ambiente para a ferramenta de backup trabalhar. Ele cria o diretório backup-tool e configura os outros aquivos e pastas necessários.

---
+++ start.sh
@@ -0,0 +1,6 @@
+mkdir -v ~/backup-tool
+cp -rv sources ~/backup-tool
+mv -v ~/backup-tool/sources ~/backup-tool/.sources
+mv -v ~/backup-tool/.sources/bchome.sh ~/backup-tool/
+mv -v ~/backup-tool/.sources/gitignore ~/backup-tool/
+mv -v ~/backup-tool/gitignore ~/backup-tool/.gitignore
