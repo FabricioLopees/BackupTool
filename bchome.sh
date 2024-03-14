519d1e048464a3b731400689955b061c6de2fc14
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-03-13 21:26:23 -0300

    build: adiciona a função de criar a cópia da home e gerar o arquivo tar.xz
    Este commit remove todas as funções implementadas anteriormente. Refaz a função de cópia, que gera o pacote tar.xz de backup da home.

--- bchome.sh
+++ bchome.sh
@@ -1,48 +1,29 @@
-# Define o caminho para a pasta home
-homedir=""
+# Acessa o nome do usuário
+username=$(echo $USER)
 
-# Define a url do repositório remoto
-remote_repo=""
-# ----------------------------------------------------------------------
-# Cria um diretório temporário para colocar a cópia da home
-mkdir ./backup/copia
+# Define o caminho da pasta home
+homedir="/home/$username/*"
 
-# Faz uma nova cópia
-cp -r $homedir ./backup/copia
+# cria uma pasta para trabalho temporária
+mkdir copydir
 
-# Salva o nome da pasta copiada
-homename=$(ls ./backup/copia/)
+# copia a pasta home para dentro da pasta de trabalho temporária
+cp -r $homedir copydir/
 
-# Gera um pacote TAR comprimido em GZIP de todos os arquivos presentes na pasta copiada
-tar -czf ./backup/$homename-home-backup.tar.gz ./backup/copia/$homename/*
+# acessa a pasta
+cd copydir
 
-# Remove a pasta de cópia temporária
-sudo rm -r ./backup/copia
+# cria um tar.xz de todos os arquivos copiados
+tar -cJf $username-home-backup.tar.xz ./*
 
-# Busca qual foi a última versão do backup
-lastVersion=$(cat ./sources/backup_lastVersion.txt)
+# move o tar.xz para um diretório acima
+mv $username-home-backup.tar.xz ../
 
-# Gera a nova versão do commit que será feito
-newVersion=$(($lastVersion+1))
+# sai da pasta temporária
+cd ..
 
-if [ ! -d "./.git" ]; then
-	# Inicia o git
-	git init > ./sources/gitlog.txt
-	
-	# Adiciona o repositório remoto
-	git remote add origin $remote_repo >> ./sources/gitlog.txt
-fi
+# exclui a pasta temporária
+sudo rm -r copydir
 
-# Adiciona as mudanças para serem comitadas
-git add ./backup >> ./sources/gitlog.txt
-
-# Faz o commit incluindo a nova versão na mensagem do commit
-git commit -m "backup: /home/$homename v$newVersion.0" >> ./sources/gitlog.txt
-
-# Manda o novo backup para o repositório remoto
-git push origin main >> ./sources/gitlog.txt
-
-# Atualiza a versão do backup salvo no arquivo
-echo $newVersion > ./sources/backup_lastVersion.txt
-
-echo "Backup do diretório /home/$homename feito!"
+# executa o script de versionamento
+. .sources/versioning.sh
