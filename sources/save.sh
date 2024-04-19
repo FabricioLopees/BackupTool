1cd35a07afe0dbd4066a6b940816a001800ae21c
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-04-18 22:41:11 -0300

    remove option to save old backups

--- sources/save.sh
+++ sources/save.sh
@@ -21,40 +21,29 @@ else
     # define o caminho do dispositivo montado
     backup_device_path="/run/media/$username/$uuid"
 
-# verifica se não esxiste os diretórios de backup no dispositivo
-if [ ! -d "$backup_device_path/backup" ]; then
-    # cria as pastas necessárias para fazer o backup
-    mkdir $backup_device_path/backup/
-    mkdir $backup_device_path/backup/$machine/
-    mkdir $backup_device_path/backup/$machine/home_backup/
+    # verifica se não esxiste os diretórios de backup no dispositivo
+    if [ ! -d "$backup_device_path/backup" ]; then
+        # cria as pastas necessárias para fazer o backup
+        mkdir $backup_device_path/backup/
+        mkdir $backup_device_path/backup/$machine/
+    fi
 
-    # cria uma pasta para colocar backup anterior
-    mkdir $backup_device_path/backup/$machine/home_backup/.old_backup
-fi
+    # define o caminho para ser colocado os arquivos de backup
+    backup_path=$backup_device_path/backup/$machine/
 
-# define o caminho para ser colocado os arquivos de backup
-backup_path=$backup_device_path/backup/$machine/home_backup/
-
-# move o backup antigo do dispositivo para a pasta .old_backup
-if [ -f "$backup_path/$username-home-backup.tar.gzip" ]; then
-    mv $backup_path/$username-home-backup.tar.gzip $backup_path/$username-home-backup-OLD.tar.gzip
-    mv $backup_path/$username-home-backup-OLD.tar.gzip $backup_path/.old_backup/
-
-    if [ -d "$backup_path/.old_backup/.git" ]; then
-        rm -rf $backup_path/.old_backup/.git/
+    # remove backup anterior se existir
+    if [ -f "$backup_path/$username-$bctype-backup.tar.gzip" ]; then
+        rm -f $backup_path/$username-$bctype-backup.tar.gzip
     fi
 
     # copia para dentro do dispositvo o arquivo de backup
     cp ~/.backup/$username-$bctype-backup.tar.gzip $backup_path/
 
-# copia para dentro do dispositvo o arquivo de backup
-cp -r $username-home-backup.tar.gzip $backup_path/
-# copia a .git que armazena o versionamento do backup
-cp -r .git/ $backup_path
+    # desmonta o dispositivo
+    udisksctl unmount -b $backup_device > /dev/null
+    echo "Desmontando dispositivo..."
 
-# desmonta o dispositivo
-udisksctl unmount -b $backup_device > /dev/null
-echo "Desmontando dispositivo..."
+    rm -r ~/.backup/$username-$bctype-backup.tar.gzip
 
     # echo "BACKUP REALIZADO COM SUCESSO!"
 fi
